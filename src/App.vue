16a0870a2a3cb7c2a14e6dea68ed5916a8df3569
Giovani Vaitkevicius <giovanivaitkevicius@hotmail.com>
2021-06-14 16:44:09 -0300

    final commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,71 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
+		<task-progress :progress="progress"/>
+		<NewTask @taskAdded="addTask"/>
+		<TaskGrid @taskDeleted="deleteTask" 
+		          :tasks="tasks"
+		          @taskStateChanged="toggleTaksState"/>
 	</div>
 </template>
 
 <script>
-export default {
+import TaskProgress from './components/TaskProgess.vue'
+import NewTask from './components/NewTask.vue'
+import TaskGrid from './components/TaskGrid.vue'
 
+export default {
+	components: {TaskGrid, NewTask,TaskProgress},
+      data(){
+		  return {
+			  tasks: []
+		  }
+	  },
+	  computed: {
+		  progress(){
+			  const total = this.tasks.length
+			  const done = this.tasks.filter(t => !t.pending).length
+			  return Math.round(done / total * 100) || 0
+		  }
+	  },
+	  watch: {
+		  tasks: {
+			  deep: true,
+			  handler(){
+				  localStorage.setItem('tasks', JSON.stringify(this.tasks))
+			  }
+		  }
+
+	  },
+	  methods: {
+		  addTask(task){
+			  const sameName = t => t.name === task.name
+			  const reallyNew = this.tasks.filter(sameName).length == 0
+			  if(reallyNew){
+				  this.tasks.push({
+					  name: task.name,
+					  pending: task.pending || true
+				  })
+			  }
+		  },
+		  deleteTask(i){
+			  this.tasks.splice(i, 1)
+		  },
+		  toggleTaksState(i){
+			  this.tasks[i].pending = !this.tasks[i].pending
+		  }
+	  },
+	  created(){
+		  const json = localStorage.getItem('tasks')
+		  const array = JSON.parse(json)
+		  if(Array.isArray(array)){
+            this.tasks = JSON.parse(json) 
+		  }
+		  else {
+			  this.tasks = []
+		  }
+		  
+	  }
 }
 </script>
 
